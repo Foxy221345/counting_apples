41e4e8aae3b8b2f8b89da189592a775a61f15d76
Foxy22 <85758375+Foxy221345@users.noreply.github.com>
2022-12-16 19:45:08 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 x = 0;
 y = 0;
-
+screen_width=0;
+screen_height=0;
 draw_apple = "";
+apple="";
+speak_data="";
+to_number=0;
 
 var SpeechRecognition = window.webkitSpeechRecognition;
   
 var recognition = new SpeechRecognition();
+
+function preload()
+{
+  apple=loadImage("apple.png");
+}
 
 function start()
 {
@@ -14,24 +23,23 @@ function start()
 } 
  
 recognition.onresult = function(event) {
+ console.log(event);
+  content=event.results[0][0].transcript;
+  document.getElementById("status").innerHTML="The speech has been recognized :" + content;
 
-  content="";
   to_number=Number(content);
 
   if(Number.isInteger(to_number))
   {
     document.getElementById("status").innerHTML="Started drawing apple";
-    draw_apple="set"
+    draw_apple="set";
   }
   else
   {
     document.getElementById("status").innerHTML="The speech has not recognized a number";
   }
- console.log(event); 
 
- content = event.results[0][0].transcript;
-
-    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+ 
 
 }
 
@@ -40,22 +48,24 @@ function setup()
  screen_width=window.innerWidth;
  screen_height=window.innerHeight;
  canvas=createCanvas(screen_width,screen_height-150);
- canvas.position(0,150)
+ canvas.position(0,150);
 }
 
 function draw() {
   if(draw_apple == "set")
   {
-    draw_apple = "";
+    
 
-    for(i=1;i<=to_number;i++)
+    for(var i=1;i<=to_number;i++)
     {
       x=Math.floor(Math.random()*700);
       y=Math.floor(Math.random()*400);
       image(apple,x,y,50,50);
     }
     document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    speak_data=to_number + "Apples drawn";
     speak();
+    draw_apple="";
   }
 }
 
@@ -69,12 +79,5 @@ function speak(){
     speak_data = "";
 }
 
-screen_width=0;
-screen_height=0;
-apple="";
-speak_data="";
 
-function preload()
-{
-  apple=loadImage("apple.png");
-}
+
